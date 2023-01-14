1cf67524783163a7a6d34019a0a580db80ee1c66
nabilHaouam <workmailnabil@gmail.com>
2023-01-14 23:09:18 +0100

    job done , the bot works, its send new tasks from the subreddit slavelabour to a telegram channel

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const Reddit = require('reddit')
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
 
 const reddit = new Reddit({
   username: 'duke_dev',
@@ -10,16 +13,18 @@ const reddit = new Reddit({
 const date = new Date()
 const timeMilliseconds = date.getTime()
 const timeSeconds = timeMilliseconds /1000
-const sub = reddit.get('/r/slavelabour/')
+reddit.get('/r/slavelabour/')
     .then((res)=>{
         const children = res.data.children;
         
         for(const child of children){
-            if (timeSeconds - child.data.created_utc <= 120 && child.data.link_flair_text == "Task"){
-                const telegramMsg = `title: ${child.data.title} \n description : ${child.data.selftext} \n link : ${child.data.url}`
+            if (timeSeconds - child.data.created_utc <= 60 && child.data.link_flair_text == "Task"){
+                const telegramMsg = `<b>Title: ${child.data.title}</b> \n Description : ${child.data.selftext} \n Link : ${child.data.url}`
+                bot.sendMessage(-1001580329503,telegramMsg, {parse_mode: "HTML"}).catch(err=> console.log(err)).then(()=>process.exit());   
                 console.log(telegramMsg) //this log is to be removed once I write the code to send the message with telegram 
             }   
         }
-    })     
+        
+    }) 
     .catch((err)=> console.log(err))
 
